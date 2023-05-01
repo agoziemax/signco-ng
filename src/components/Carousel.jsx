35e01df96f1cb5d3530d225e4e1e84c489dcfbc9
Agoziemax <agoziemax@yahoo.com>
2023-05-01 23:57:01 +0100

    Added react-bootstrap

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,47 +1,25 @@
 import React from 'react'
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Caro from '../assets/bg.JPG'
-
-
-function Carousel() {
-
+import Carousel from 'react-bootstrap/Carousel';
+import Bg from '../assets/banner1.jpg';
+import Bg2 from '../assets/banner3.jpg';
+import Bg3 from '../assets/banner7.jpg';
+function CarouselFade() {
   return (
     <div>
-      <div id='carouselExampleFade' className='carousel slide carousel-fade'>
-        <div className='carousel-inner'>
-          <div className='carousel-item active'>
-            <img src={Caro} className='d-block w-100' alt='...' />
-          </div>
-          <div className='carousel-item'>
-            <img src='...' className='d-block w-100' alt='...' />
-          </div>
-          <div className='carousel-item'>
-            <img src='...' className='d-block w-100' alt='...' />
-          </div>
-        </div>
-        <button
-          className='carousel-control-prev'
-          type='button'
-          data-bs-target='#carouselExampleFade'
-          data-bs-slide='prev'
-        >
-          <span className='carousel-control-prev-icon' aria-hidden='true' />
-          <span className='visually-hidden'>Previous</span>
-        </button>
-        <button
-          className='carousel-control-next'
-          type='button'
-          data-bs-target='#carouselExampleFade'
-          data-bs-slide='next'
-        >
-          <span className='carousel-control-next-icon' aria-hidden='true' />
-          <span className='visually-hidden'>Next</span>
-        </button>
-      </div>
+      <Carousel fade>
+        <Carousel.Item>
+          <img className='d-block w-100' src={Bg2} alt='First slide' />
+
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className='d-block w-100' src={Bg} alt='Second slide' />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className='d-block w-100' src={Bg3} alt='Third slide' />
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
 
-export default Carousel
+export default CarouselFade;
